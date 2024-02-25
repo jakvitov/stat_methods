cb264c3d6f3f38715ad2246bf23d1b73b5adb6fa
jakvitov <jakvitov@gmail.com>
2024-02-25 17:19:11 +0100

    Added function for computing Cramer and Pearson dependence

--- categorical_dependence_test.R
+++ categorical_dependence_test.R
@@ -22,14 +22,26 @@ test_criteria = function(real, theoretical){
       tc = tc + ((real[i, j] - theoretical[i, j])^2 / theoretical[i, j])
     }
   }
+  
   return(tc)
+}
+
+#Return value of pearson dependency coefficient for the given values
+pearson_dependence_coefficient = function(G, n) {
+  return(sqrt(G/(n+G)))
+}
+
+#Return value of Cramer dependency coefficient for the given values
+cramer_dependence_coefficient = function(G, n, m) {
+  return(sqrt(G/(n*(m-1))))
 }
 
 #Given matrix representing values of two categorical variables calculate on the given 
 # significance level alpha if they are dependent or not
-# Return false if H0 is accepted (they are not dependent)
-# Return true if H0 is not accepted in favour of H1
+#In case of dependece, return vector with pearson and cramer dependency coefficient
+#Else return vector of nulls
 categorical_dependence = function(mtx, alpha) {
+  res = c(0,0)
   #First we calculate hypothetical distribution matrix
   r_sum=rowSums(mtx)
   emtx = cbind(mtx, r_sum)
@@ -39,11 +51,17 @@ categorical_dependence = function(mtx, alpha) {
   
   tc = test_criteria(emtx, theor_dist)
   
-  crit_int=qchisq(1-alpha, (nrow(mtx) - 1) *(ncol(mtx) - 1))
-  print(tc)
-  return(FALSE)
-  
+  crit_int=qchisq(alpha, (nrow(mtx) - 1) *(ncol(mtx) - 1))
+  # H0 is rejected (dependecy exists on alpha significance level)
+  if (tc > crit_int) {
+      res[0] = pearson_dependence_coefficient(tc, emtx[nrow(emtx), ncol(emtx)])
+      res[1] = cramer_dependence_coefficient(tc, emtx[nrow(emtx), ncol(emtx)], nrow(mtx))
+      return(res)
+  }
+  return(res)
 }
 
+
+
 test_data = matrix(c(22, 11, 37, 42, 10, 8), nrow=2, ncol=3, byrow=TRUE)
-categorical_dependence(test_data, 0.95)
+print(categorical_dependence(test_data, 0.95))
