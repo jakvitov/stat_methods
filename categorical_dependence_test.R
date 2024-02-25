e210b70e09353dd34e1b28371bc10dd57560712d
jakvitov <jakvitov@gmail.com>
2024-02-25 16:42:42 +0100

    Chi squared test implementecd

--- categorical_dependence_test.R
+++ categorical_dependence_test.R
@@ -7,11 +7,22 @@ theor_dist_matrix = function(mtx){
     for (j in 1:(ncol(mtx)-1)) {
       res[i,j] = (mtx[i, ncol(mtx)] * mtx[nrow(mtx), j])/n
       if (res[i, j] < 5) {
-        return FALSE
+        stop("Cannot use Chi-quadrat because of small number of theoretical distributions. Need at least five of each.")
       }
     }
   }
   return(res)
+}
+
+#Return test criteria mased on the given matrices used for chi quadrat
+test_criteria = function(real, theoretical){
+  tc = 0
+  for (i in 1:(nrow(real)-1)) {
+    for (j in 1:(ncol(real)-1)) {
+      tc = tc + ((real[i, j] - theoretical[i, j])^2 / theoretical[i, j])
+    }
+  }
+  return(tc)
 }
 
 #Given matrix representing values of two categorical variables calculate on the given 
@@ -24,9 +35,14 @@ categorical_dependence = function(mtx, alpha) {
   emtx = cbind(mtx, r_sum)
   c_sum=colSums(emtx)
   emtx = rbind(emtx, c_sum)
-  print(emtx)
-  print(theor_dist_matrix(emtx))
+  theor_dist = theor_dist_matrix(emtx)
+  
+  tc = test_criteria(emtx, theor_dist)
+  
+  crit_int=qchisq(1-alpha, (nrow(mtx) - 1) *(ncol(mtx) - 1))
+  print(tc)
   return(FALSE)
+  
 }
 
 test_data = matrix(c(22, 11, 37, 42, 10, 8), nrow=2, ncol=3, byrow=TRUE)
