d2c08942814e0127d5c637e50390e49f05232043
jakvitov <jakvitov@gmail.com>
2024-03-12 20:36:27 +0100

    Model fix

--- linear_regression.jl
+++ linear_regression.jl
@@ -1,35 +1,35 @@
 using Plots
 
 #This implementation uses general statistic formula for two vaiable linear regression
-#Genearalization for any number of parameters can be found in my different repository here 
+#Genearalization for any Float64 of parameters can be found in my different repository here 
 #https://github.com/jakvitov/math_simulations/blob/main/gradient_descend.ipynb
 #That features full jupyter notebook with both math and algorithm explanation in Czech
 
 #Julia implementation of simple linear regression model 
 mutable struct RegressionModel
     #Parameters of the given model, parameter at ith position represents (i-1)th variable direction, position 1 is for constant
-    parameters::Vector{Real}
-    x_in::Vector{Float64}
-    y_in::Vector{Float64}
+    parameters::Array{Real}
+    x_in::Array{Float64}
+    y_in::Array{Float64}
 end
 
 #Avoiding Statistics package 
-function vector_mean(vec::Vector{Number})::Real
+function Array_mean(vec::Array{Float64})::Real
     return (sum(vec)//length(vec))
 end
 
 #Get direction parameter of b1
-function get_first_direction(x_in::Vector{Number}, y_in::Number{Float64})::Real
-    return (vector_mean(x_in.* y_in)-(vector_mean(x_in)*vector_mean(y_in)))//(vector_mean(x_in.^2)-(vector_mean(x_in)^2))
+function get_first_direction(x_in::Array{Float64}, y_in::Array{Float64})::Real
+    return (Array_mean(x_in.* y_in)-(Array_mean(x_in)*Array_mean(y_in)))//(Array_mean(x_in.^2)-(Array_mean(x_in).^2))
 end
 
 #Get coefficient b0
-function get_coeficient(x_in::Vector{Number}, y_in::Vector{Number}, b1::Real)::Real
-    return (vector_mean(y_in) - (vector_mean(x_in).* b1))
+function get_coeficient(x_in::Array{Float64}, y_in::Array{Float64}, b1::Real)::Real
+    return (Array_mean(y_in) - (Array_mean(x_in).* b1))
 end
 
-function model_init(x_in::Vector{Number}, y_in::Vector{Number})
-    model::RegressionModel = RegressionModel
+function model_init(x_in::Array{Float64}, y_in::Array{Float64})
+    model::RegressionModel = RegressionModel()
     model.x_in = deepcopy(x_in)
     model.y_in = deepcopy(y_in)
     b1 = get_first_direction(x_in, y_in)
@@ -39,7 +39,7 @@ function model_init(x_in::Vector{Number}, y_in::Vector{Number})
 end
 
 #Predict y value based on the given model
-function predict(model::RegressionModel, x_val::Number)
+function predict(model::RegressionModel, x_val::Float64)
     return model.parameters[1]*x_val + model.parameters[0]
 end
 
@@ -54,5 +54,6 @@ end
 
 test_data_x = reshape(1:50, 50, 1)
 test_data_y = test_data_x.* rand(0:10, length(test_data_x))
-model = model_init(test_data_x, test_data_y)
-plot_model(model)
+typeof(test_data_x)
+#model = model_init(test_data_x, test_data_y)
+#plot_model(model)
